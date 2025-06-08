fdd776435738c3c930b567e2f3c4afcce9a19886
Jen Planque <jennyleeplanque@gmail.com>
2025-06-07 17:44:02 -0700

    test upload

--- app/models.py
+++ app/models.py
@@ -73,3 +73,6 @@ class ServiceTicket(Base):
     mechanics: Mapped[List["Mechanic"]] = db.relationship(
         "Mechanic", secondary=service_mechanics, back_populates="service_tickets"
     )
+
+
+#   test

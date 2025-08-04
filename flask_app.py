ce498c6f2b93d37162104c9e9be2fb97cd4891fb
Jen Planque <jennyleeplanque@gmail.com>
2025-08-04 19:46:54 -0400

    push repository before starting deployment process with Render

--- flask_app.py
+++ flask_app.py
@@ -12,4 +12,4 @@ with app.app_context():
     # db.reflect()  # Reflect existing tables
     # db.drop_all()  # Drop all sql tables if needed
     db.create_all()
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True)
